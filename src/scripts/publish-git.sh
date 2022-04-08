3c8b49bb380f4b5a6443b27b5400011db8ed34b5
Matt Taylor <mtaylor@chime.com>
2022-04-08 10:01:16 -0700

    shell check nits

--- src/scripts/publish-git.sh
+++ src/scripts/publish-git.sh
@@ -27,9 +27,8 @@ git tag -a $VERSION -m "$VERSION"
 git push https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GEM_NAME}.git $VERSION
 echo "Finished Tagging to git"
 
-RELEASE_BODY=$(git log -1 --pretty=format:%B ${CIRCLE_SHA1})
+RELEASE_BODY=$(git log -1 --pretty=format:%B "${CIRCLE_SHA1}")
 
-ghr -replace -t ${GITHUB_TOKEN} -u ${GITHUB_USER} -r ${GEM_NAME} \
-    -c ${CIRCLE_SHA1} -n "${VERSION}" -b "${RELEASE_BODY}" ${VERSION} $FILE
-
+ghr -replace -t "${GITHUB_TOKEN}" -u "${GITHUB_USER}" -r "${GEM_NAME}" \
+    -c "${CIRCLE_SHA1}" -n "${VERSION}" -b "${RELEASE_BODY}" "${VERSION}" "$FILE"
 echo "Finished Releasing to git"
