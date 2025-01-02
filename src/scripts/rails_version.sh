e6bd13dfa2ad7cd69ff0de3f26ca69ad11cb498e
Matt Taylor <3680737+matt-taylor@users.noreply.github.com>
2025-01-01 17:09:24 -0800

    Fix Rails check [semver:minor] (#13)
    * use raw ruby instead of parsing the Gemfile.lock
    
    * run it in ruby
    
    * executable
    
    * set shell version
    
    * go back to shell version with ruby bindings
    
    * fix spellcheck
    
    * simplify it
    
    * remove ruby version
    
    * quote it
    
    * undo the things..remove spellcheck

--- src/scripts/rails_version.sh
+++ src/scripts/rails_version.sh
@@ -1,9 +1,14 @@
 
-gemlock_rails=$(grep "^  rails (" Gemfile.lock)
+CURRENT_RAILS_VERSION=$(bundle exec gem list rails | grep -i "^rails (" | tr -cd "0-9.")
+COMMAND="require \"bundler\";"\
+"dependency = Gem::Dependency.new(\"rails\", Gem::Requirement.new([ENV.fetch(\"EXPECTED_RAILS_VERSION\")]), :runtime);"\
+"puts dependency.match?(\"rails\", ENV[\"CURRENT_RAILS_VERSION\"]) ==  nil"
 
-if [[ "$gemlock_rails" == *"$EXPECTED_RAILS_VERSION"* ]]; then
+dependency_result=$(CURRENT_RAILS_VERSION=$CURRENT_RAILS_VERSION EXPECTED_RAILS_VERSION="~> 7" ruby -e "$COMMAND")
+
+if [[ "$dependency_result" == "false" ]]; then
   echo "Gemfile.lock Rails version matches the expected rails version"
 else
-  echo "Yikes! found rails [$gemlock_rails]. Expected $EXPECTED_RAILS_VERSION. Something went wrong. Exiting"
+  echo "Yikes! found rails [$CURRENT_RAILS_VERSION]. Expected $EXPECTED_RAILS_VERSION. Something went wrong. Exiting"
   exit 1
 fi
